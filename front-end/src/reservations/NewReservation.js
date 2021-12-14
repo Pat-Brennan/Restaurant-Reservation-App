103e2773ea76a9d234ef23564fe2eb1429e34aff
Patrick Brennan <patbrennan7@gmail.com>
2021-12-14 13:21:09 -0500

    fixed form bug

--- front-end/src/reservations/NewReservation.js
+++ front-end/src/reservations/NewReservation.js
@@ -23,6 +23,10 @@ function NewReservation() {
         });
     }
 
+    const cancelHandler = () => {
+        history.goBack();
+    }
+
     const submitHandler = (event) => {
         event.preventDefault();
         newReservation.people = Number(newReservation.people);
@@ -35,85 +39,94 @@ function NewReservation() {
 
     return (
         <>
-            <ErrorAlert error={error} />
-            <form onSubmit={submitHandler}>
-
-                <div>
-                <label htmlFor="first_name">First Name:</label>
+        <h1>Create Reservation</h1>
+        <ErrorAlert error={error} />
+        <form onSubmit={submitHandler}>
+            <div>
+                <label htmlFor="first_name" className="form-label">First Name</label>
                 <input
                     type="text"
-                    placeholder="First Name Here"
                     name="first_name"
+                    className="form-control"
                     id="first_name"
+                    placeholder="First Name Here"
                     value={newReservation.first_name}
                     onChange={changeHandler}
                     required
                 />
-                </div>
-                <div>
-                    <label htmlFor="last_name">Last Name:</label>
-                    <input
-                        type="text"
-                        placeholder="Last Name Here"
-                        name="last_name"
-                        id="last_name"
-                        value={newReservation.last_name}
-                        onChange={changeHandler}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="mobile_number"> Mobile Number:</label>
-                    <input
-                        type="text"
-                        placeholder="(666)666-6666"
-                        name="mobile_number"
-                        id="mobile_number"
-                        value={newReservation.mobile_number}
-                        onChange={changeHandler}
-                        required
-                    />
-                </div>
-                <div>
-                <label htmlFor="reservation_date">Date:</label>
+            </div>
+            <div>
+                <label htmlFor="first_last" className="form-label">Last Name</label>
+                <input
+                    type="text"
+                    name="last_name"
+                    className="form-control"
+                    id="last_name"
+                    placeholder="Last Name Here"
+                    value={newReservation.last_name}
+                    onChange={changeHandler}
+                    required
+                />
+            </div>
+            <div>
+                <label htmlFor="mobile_number" className="form_label">Mobile Number</label>
+                <input
+                    type="tel"
+                    name="mobile_number"
+                    className="form-control"
+                    id="mobile_number"
+                    pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                    placeholder="xxx-xxx-xxxx"
+                    value={newReservation.mobile_number}
+                    onChange={changeHandler}
+                    required
+                />
+            </div>
+            <div>
+            <label htmlFor="reservation_date" className="form-label">Date of Reservation</label>
                 <input
                     type="date"
                     name="reservation_date"
+                    className="form-control"
                     id="reservation_date"
-                    onChange={changeHandler}
+                    pattern="\d{4}-\d{2}-\d{2}"
+                    placeholder="YYYY-MM-DD"
                     value={newReservation.reservation_date}
+                    onChange={changeHandler}
                     required
                 />
-                </div>
-                <div>
-                    <label htmlFor="reservation_time">Time:</label>
-                    <input
-                        type="time"
-                        name="reservation_time"
-                        id="reservation_time"
-                        onChange={changeHandler}
-                        value={newReservation.reservation_time}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="people">Number of Guests:</label>
-                    <input
-                        type="number"
-                        name="people"
-                        id="people"
-                        onChange={changeHandler}
-                        value={newReservation.people}
-                        required
-                    />
-                </div>
+            </div>
+            <div>
+            <label htmlFor="reservation_time" className="form-label">Time of Reservation</label>
+                <input
+                    type="time"
+                    name="reservation_time"
+                    className="form-control"
+                    id="reservation_time"
+                    pattern="[0-9]{2}:[0-9]{2}"
+                    placeholder="HH:MM"
+                    value={newReservation.reservation_time}
+                    onChange={changeHandler}
+                    required
+                />
+            </div>
+            <div>
+            <label htmlFor="people" className="form-label">Party Size</label>
+                <input
+                    type="number"
+                    name="people"
+                    className="form-control"
+                    id="people"
+                    min="1"
+                    value={newReservation.people}
+                    onChange={changeHandler}
+                    required
+                />
+            </div>
+            <div>
                 <button type="submit" className="btn btn-primary">Submit</button>
-                <button
-                    type="cancel"
-                    className="btn btn-danger"
-                    onClick={() => history.goBack()}>
-                    Cancel
-                </button>
+                <button type="button" className="btn btn-danger" onClick={cancelHandler}>Cancel</button>
+            </div>
         </form>
     </>
     )
